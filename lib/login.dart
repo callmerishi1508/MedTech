d3bc58526e5314059d8747f633674931cbe4526a
Rishi <rishivardhanreddy8055@gmail.com>
2023-12-13 17:27:12 +0530

    Basic Page Created

---
+++ lib/login.dart
@@ -0,0 +1,84 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_project/main.dart';
+import 'package:firebase_project/signuppage.dart';
+import 'package:firebase_project/uihelper.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  login(String email, String password) async {
+    //verified
+    if (email == "" && password == "") {
+      return UiHelper.CustomAlertBox(context, "Enter Required Fields");
+    } else {
+      UserCredential? userCredential;
+    }
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email, password: password);
+      {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: ((context) => MyHomePage(title: "Login"))));
+      }
+      ;
+    } on FirebaseAuthException catch (ex) {
+      return UiHelper.CustomAlertBox(context, ex.code.toString());
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login Page'),
+        centerTitle: true,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          UiHelper.CustomTextField(emailController, "Email", Icons.mail, false),
+          UiHelper.CustomTextField(
+              passwordController, "Password", Icons.password, true),
+          SizedBox(height: 30),
+          UiHelper.CustomButtom(() {
+            login(emailController.text.toString(),
+                passwordController.text.toString());
+          }, "Login"),
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Already Have an Account?",
+                style: TextStyle(fontSize: 16),
+              ),
+              TextButton(
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => SignUPPage()));
+                  },
+                  child: Text(
+                    "Sign Up",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ))
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/signuppage.dart
@@ -0,0 +1,63 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_project/main.dart';
+import 'package:firebase_project/uihelper.dart';
+//import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class SignUPPage extends StatefulWidget {
+  const SignUPPage({super.key});
+
+  @override
+  State<SignUPPage> createState() => _SignUPPageState();
+}
+
+class _SignUPPageState extends State<SignUPPage> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  signUp(String email, String password) async {
+    if (email == "" && password == "") {
+      return UiHelper.CustomAlertBox(context, "Enter Required Fields");
+    } else {
+      UserCredential? usercredential;
+      try {
+        usercredential = await FirebaseAuth.instance
+            .createUserWithEmailAndPassword(email: email, password: password)
+            .then((value) {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => MyHomePage(title: "HomePage")));
+        });
+      } on FirebaseAuthException catch (ex) {
+        return UiHelper.CustomAlertBox(context, ex.code.toString());
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Sign Up Page"),
+        centerTitle: true,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          UiHelper.CustomTextField(emailController, "Email", Icons.mail, false),
+          UiHelper.CustomTextField(
+              passwordController, "Password", Icons.password, true),
+          SizedBox(height: 30),
+          UiHelper.CustomButtom(() {
+            signUp(emailController.text.toString(),
+                passwordController.text.toString());
+          }, "Sign Up")
+        ],
+      ),
+    );
+  }
+}
